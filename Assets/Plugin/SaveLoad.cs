c6fbb13e8299289141a1aa5fdbe385813f4ab93f
medhat <medhat.fayed@ontariotechu.net>
2019-09-26 15:56:37 -0400

    Plugin and Script update

---
+++ Assets/Plugin/SaveLoad.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+public class SaveLoad : MonoBehaviour
+{
+    const string DLL_NAME = "Plugin 1";
+    [DllImport(DLL_NAME)]
+    private static extern int save(float xPosition, float yPosition, float zPosition);
+    [DllImport(DLL_NAME)]
+    private static extern Vector3 load();
+
+    Vector3 location;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            float x = transform.localPosition.x;
+            float y = transform.localPosition.y;
+            float z = transform.localPosition.z;
+
+            save(x, y, z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            location = load();
+            transform.localPosition = location;
+        }
+    }
+}
